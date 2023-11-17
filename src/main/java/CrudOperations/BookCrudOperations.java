61ffc728ae48111176254a77f2cf6ef84fc90bbc
Harenabs21 <hei.harena.6@gmail.com>
2023-11-17 12:00:06 +0300

    feat: create the BookCrudOperations for Book

---
+++ src/main/java/CrudOperations/BookCrudOperations.java
@@ -0,0 +1,33 @@
+package CrudOperations;
+
+import Interface.CrudOperations;
+
+import java.awt.print.Book;
+import java.util.List;
+
+public class BookCrudOperations implements CrudOperations<Book> {
+
+    @Override
+    public List<Book> findAll() {
+        return null;
+        //TODO
+    }
+
+    @Override
+    public List<Book> saveAll(List<Book> toSave) {
+        return null;
+        //TODO
+    }
+
+    @Override
+    public Book save(Book toSave) {
+        return null;
+        //TODO
+    }
+
+    @Override
+    public Book delete(Book toDelete) {
+        return null;
+        //TODO
+    }
+}
