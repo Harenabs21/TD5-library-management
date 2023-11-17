1f55925c7c0755eb6f4d802c12a90017e6047aaf
Harenabs21 <hei.harena.6@gmail.com>
2023-11-17 11:46:57 +0300

    feat: create the Visitors class

---
+++ src/main/java/Visitors.java
@@ -0,0 +1,47 @@
+public class Visitors {
+    private int id;
+    private String name;
+    private String reference;
+
+    public Visitors(int id, String name, String reference) {
+        this.id = id;
+        this.name = name;
+        this.reference = reference;
+    }
+    public Visitors(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    @Override
+    public String toString() {
+        return "Visitors{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", reference='" + reference + '\'' +
+                '}';
+    }
+}
