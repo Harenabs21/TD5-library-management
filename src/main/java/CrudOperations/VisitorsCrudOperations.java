280f5358e0bfeba0d1ee0a7a5520bf877a38e3da
Harenabs21 <hei.harena.6@gmail.com>
2023-11-17 12:32:38 +0300

    feat: create the VisitorsCrudOperations class for visitors

---
+++ src/main/java/CrudOperations/VisitorsCrudOperations.java
@@ -0,0 +1,28 @@
+package CrudOperations;
+
+import Interface.CrudOperations;
+import Model.Visitors;
+
+import java.util.List;
+
+public class VisitorsCrudOperations implements CrudOperations<Visitors> {
+    @Override
+    public List<Visitors> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<Visitors> saveAll(List<Visitors> toSave) {
+        return null;
+    }
+
+    @Override
+    public Visitors save(Visitors toSave) {
+        return null;
+    }
+
+    @Override
+    public Visitors delete(Visitors toDelete) {
+        return null;
+    }
+}
