64832914b209e67d40e2ce257272ac3748e728e3
Harenabs21 <hei.harena.6@gmail.com>
2023-11-17 14:12:14 +0300

    fix: use the lombok for  more clarity in the syntax

--- src/main/java/Model/Author.java
+++ src/main/java/Model/Author.java
@@ -1,48 +1,15 @@
 package Model;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Author {
     private int id;
     private String name;
     private char sex;
 
-    public Author(int id, String name, char sex) {
-        this.id = id;
-        this.name = name;
-        this.sex = sex;
-    }
-    public Author(){
-
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public char getSex() {
-        return sex;
-    }
-
-    public void setSex(char sex) {
-        this.sex = sex;
-    }
-
-    @Override
-    public String toString() {
-        return "Authors{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", sex=" + sex +
-                '}';
-    }
 }
--- src/main/java/Model/Book.java
+++ src/main/java/Model/Book.java
@@ -1,6 +1,12 @@
 package Model;
-import java.sql.Date;
+import lombok.*;
 
+import java.sql.Date;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Book {
     private int id;
     private String bookName;
@@ -8,68 +14,5 @@ public class Book {
     private String topic;
     private Date releaseDate;
 
-    public Book(int id, String bookName, int pageNumbers, String topic, Date releaseDate) {
-        this.id = id;
-        this.bookName = bookName;
-        this.pageNumbers = pageNumbers;
-        this.topic = topic;
-        this.releaseDate = releaseDate;
-    }
 
-    public Book() {
-
-    }
-
-
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getBookName() {
-        return bookName;
-    }
-
-    public void setBookName(String bookName) {
-        this.bookName = bookName;
-    }
-
-    public int getPageNumbers() {
-        return pageNumbers;
-    }
-
-    public void setPageNumbers(int pageNumbers) {
-        this.pageNumbers = pageNumbers;
-    }
-
-    public String getTopic() {
-        return topic;
-    }
-
-    public void setTopic(String topic) {
-        this.topic = topic;
-    }
-
-    public Date getReleaseDate() {
-        return releaseDate;
-    }
-
-    public void setReleaseDate(Date releaseDate) {
-        this.releaseDate = releaseDate;
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "id=" + id +
-                ", bookName='" + bookName + '\'' +
-                ", pageNumbers=" + pageNumbers +
-                ", topic='" + topic + '\'' +
-                ", releaseDate=" + releaseDate +
-                '}';
-    }
 }
