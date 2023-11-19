ca1ff922ead3a546b2e0f693800dee295ecf659a
Harenabs21 <hei.harena.6@gmail.com>
2023-11-19 13:51:57 +0300

    feat: add insert datas for the subscribers

--- src/main/resources/create_table_subscribers.sql
+++ src/main/resources/create_table_subscribers.sql
@@ -3,3 +3,14 @@ CREATE TABLE IF NOT EXISTS visitor(
     name VARCHAR(200) NOT NULL ,
     reference VARCHAR(200) NOT NULL
 )
+-- Premier visiteur
+INSERT INTO visitor (id, name, reference)
+VALUES (1, 'Alice Wonderland', 'ABC123');
+
+-- Deuxième visiteur
+INSERT INTO visitor (id, name, reference)
+VALUES (2, 'Bob Marley', 'XYZ789');
+
+-- Troisième visiteur
+INSERT INTO visitor (id, name, reference)
+VALUES (3, 'Charlie Chaplin', 'PQR456');
